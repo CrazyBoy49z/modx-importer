3f1959f8dddefdc6481d14a21754d44a7b475de9
Hunter Kempton <hunter@Hunters-MacBook-Pro.local>
2018-04-14 14:13:01 -0400

    created db connection

---
+++ env.js
@@ -0,0 +1,7 @@
+var dbUser = 'root';
+var dbPassword = 'root';
+var dbName = 'modx';
+
+module.exports.dbUser = dbUser;
+module.exports.dbName = dbName;
+module.exports.dbPassword = dbPassword;
--- server.js
+++ server.js
@@ -1,5 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var app = express();
-var converter = require('./env');
+var env = require('./env.js');
+const pool = require('./env.js');
 app.use(express.static(__dirname + '/pub'));
+var mysql = require('mysql');
+
+console.log(env.dbUser);
+
+var con = mysql.createConnection({
+    user: env.dbUser,
+    password: env.dbPassword,
+    host: "localhost",
+    port: 8889,
+    database: env.dbName,
+    ssl: false
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+function errorCallback(res) {
+	return function(err) {
+		console.log(err);
+        res.status(500); // 500 Server Error
+        res.send("ERROR!");
+    }
+}
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+	console.log('JSON Server is running on ' + port);
+});
